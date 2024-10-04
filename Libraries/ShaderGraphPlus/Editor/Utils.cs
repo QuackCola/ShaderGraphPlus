dd1f57abff5f799c51e0a473f68c35f576242fc4
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-10-04 19:58:41 -0400

    Update Utils.cs

--- Libraries/ShaderGraphPlus/Editor/Utils.cs
+++ Libraries/ShaderGraphPlus/Editor/Utils.cs
@@ -1,9 +1,68 @@
-﻿using System.IO;
-
-namespace Editor.ShaderGraphPlus;
+﻿namespace Editor.ShaderGraphPlus;
 
 public static class Utils
 {
+	
+	public static class ProjectHelpers
+	{
+		/// <summary>
+		/// Uses Regex pattern matching to fetch the Ident from the project's executing assembly name.
+		/// </summary>
+		public static string GetIdentFromExecutingAssemblyName()
+		{
+			string executingAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+			string pattern = @"^[^.]+\.[^.]+\.([^.]+)\.";
+
+			Match match = Regex.Match( executingAssemblyName, pattern );
+
+			var result = "";
+
+			if ( match.Success )
+			{
+				result = match.Groups[1].Value;
+			}
+
+			return result;
+		}
+
+
+		/// <summary>
+		/// Gets the org ident of the matching library package ident.
+		/// </summary>
+		public static string GetLibraryOrgIdent( string ident )
+		{
+			var libraryOrg = "";
+
+			foreach ( var library in LibrarySystem.All )
+			{
+				if ( library.Project.Package.Ident == ident )
+				{
+					libraryOrg = library.Project.Package.Org.Ident;
+				}
+			}
+
+			return libraryOrg;
+		}
+
+		/// <summary>
+		/// Gets the matching library package ident.
+		/// </summary>
+		public static string GetLibraryPackageIdent( string ident )
+		{
+			var libraryIdent = "";
+
+			foreach ( var library in LibrarySystem.All )
+			{
+				if ( library.Project.Package.Ident == ident )
+				{
+					libraryIdent = library.Project.Package.Ident;
+				}
+			}
+			return libraryIdent;
+		}
+	}
+	
 	public static string ChooseExistingPath( string path1, string path2 )
 	{
 		if ( Directory.Exists( path1 ) )
@@ -14,8 +73,15 @@ public static class Utils
 		{
 			return path2;
 		}
+		else if ( Directory.Exists( path1 ) || Directory.Exists( path2 ) )
+		{
+			Log.Error( $"Both path 1 & path 2 exist!" );
+			return null;
+		}
 		else
+		{
 			return null; // Neither path exists
+		}
 	}
 
     // Opens a specified textfile in Notepad.
@@ -32,8 +98,24 @@ public static class Utils
     /// </summary>
     public static string GetProjectRootPath()
     {
-        return Project.Current.GetRootPath().Replace('\\', '/');
+        return GetProjectAbsolutePath();
     }
+
+	/// <summary>
+	/// Absolute path to the location of the .sbproj file of the project.
+	/// </summary>
+	public static string GetProjectAbsolutePath()
+	{
+		return Project.Current.GetRootPath().Replace( '\\', '/' );
+	}
+
+	/// <summary>
+	/// Absolute path to a file or directory thats within a mounted library project.
+	/// </summary>
+	public static string GetLibaryAbsolutePath( string path )
+	{
+		return FileSystem.Libraries.GetFullPath( path ).Replace( '\\', '/' );
+	}
 
     public static string GetProjectCodePath()
     {
