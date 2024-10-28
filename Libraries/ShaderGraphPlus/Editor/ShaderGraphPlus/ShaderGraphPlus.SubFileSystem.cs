481b22fde2f822b631b37bf4312092006c524592
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-10-28 18:33:23 -0400

    Update ShaderGraphPlus.SubFileSystem.cs

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/ShaderGraphPlus.SubFileSystem.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/ShaderGraphPlus.SubFileSystem.cs
@@ -1,33 +1,32 @@
-﻿namespace Editor.ShaderGraphPlus
+﻿namespace Editor.ShaderGraphPlus;
+
+public static class ShaderGraphPlusFileSystem
 {
-    public static class ShaderGraphPlusFileSystem
+    public static BaseFileSystem FileSystem = Editor.FileSystem.Libraries.CreateSubSystem($"/{LibraryName()}");
+
+    private static string LibraryName()
     {
-        public static BaseFileSystem FileSystem = Editor.FileSystem.Libraries.CreateSubSystem($"/{LibraryName()}");
+        var dev_name = "ShaderGraphPlus";
+        var user_name = "quack.shadergraphplus";
+        var dev_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{dev_name}";
+        var user_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{user_name}";
 
-        private static string LibraryName()
+        if (Directory.Exists(dev_path))
         {
-            var dev_name = "ShaderGraphPlus";
-            var user_name = "quack.shadergraphplus";
-            var dev_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{dev_name}";
-            var user_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{user_name}";
-
-            if (Directory.Exists(dev_path))
-            {
-                return dev_name;
-            }
-            else if (Directory.Exists(user_path))
-            {
-                return user_name;
-            }
-            else if (Directory.Exists(dev_path) || Directory.Exists(user_path))
-            {
-                Log.Error($"Both dev_path & user_path exist!");
-                return null;
-            }
-            else
-            {
-                return null; // Neither path exists
-            }
+            return dev_name;
+        }
+        else if (Directory.Exists(user_path))
+        {
+            return user_name;
+        }
+        else if (Directory.Exists(dev_path) || Directory.Exists(user_path))
+        {
+            Log.Error($"Both dev_path & user_path exist!");
+            return null;
+        }
+        else
+        {
+            return null; // Neither path exists
         }
     }
 }
