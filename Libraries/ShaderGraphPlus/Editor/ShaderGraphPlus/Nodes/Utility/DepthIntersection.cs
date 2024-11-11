5140b6a116c02ca4227f0051b502559b94201d18
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-11-11 11:10:06 -0500

    Depth Intersection Node stub

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Utility/DepthIntersection.cs
@@ -0,0 +1,29 @@
+﻿namespace Editor.ShaderGraphPlus.Nodes;
+
+
+[Title("Depth Intersection"), Category( "Utility" )]
+public sealed class DepthIntersectionNode : ShaderNodePlus
+{
+
+[Hide]
+public string DepthIntersect => @"
+float DepthIntersect( float2 vUv )
+{
+	float flDepth = Depth::Get(vUv);
+
+
+	return flDepth;
+}
+";
+
+    [Output(typeof(float))]
+    [Hide]
+    public NodeResult.Func Result => (GraphCompiler compiler) =>
+    {
+        var coords = $"i.vPositionSs.xy";
+
+        var result = compiler.ResultFunction(DepthIntersect, args: $"{coords}");
+        return new NodeResult(ResultType.Float, result);
+    };
+
+}
