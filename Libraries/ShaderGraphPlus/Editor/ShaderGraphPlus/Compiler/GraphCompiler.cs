ac4f207db48d0c872a0baeb5768d51f346ca5a88
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-03-13 06:01:32 -0400

    GetDataType function

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/GraphCompiler.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/GraphCompiler.cs
@@ -842,6 +842,24 @@ public sealed partial class GraphCompiler
 		return prefix;
     }
 
+	public string GetDataType( ResultType resultType )
+	{
+		return resultType switch
+		{
+            ResultType r when r == ResultType.Bool => "bool",
+            ResultType r when r == ResultType.Int => "int",
+            ResultType r when r == ResultType.Float => "float",
+            ResultType r when r == ResultType.Vector2 => "float2",
+            ResultType r when r == ResultType.Vector3 => "float3",
+            ResultType r when r == ResultType.Color => "float4",
+            ResultType r when r == ResultType.Gradient => "Gradient",
+            ResultType r when r == ResultType.Float2x2 => "float2x2",
+            ResultType r when r == ResultType.Float3x3 => "float3x3",
+            ResultType r when r == ResultType.Float4x4 => "float4x4",
+            _ => throw new ArgumentException("Unsupported value type", nameof(resultType))
+        };
+	}
+
 	private static object GetDefaultValue( SubgraphNode node, string name, Type type )
 	{
 		if ( !node.DefaultValues.TryGetValue( name, out var value ) )
