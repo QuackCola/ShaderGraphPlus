bee9e476a9bcb062b4f73c6168f664b0352b7432
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-04-20 06:05:11 -0400

    Add LightingResult node

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes Core/LightingResult.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Editor.ShaderGraphPlus;
+
+/// <summary>
+/// Final result of the lighting calcuations.
+/// </summary>
+[Title( "Light Result" ), Icon( "output" )]
+public sealed class LightingResult : BaseResult
+{
+	[Hide]
+	[Input( typeof( Vector3 ) )]
+	public NodeInput Albedo { get; set; }
+
+	public override NodeInput GetAlbedo()
+	{
+		if ( Albedo.IsValid )
+		{
+			return Albedo;
+		}
+		return default;
+	}
+}
