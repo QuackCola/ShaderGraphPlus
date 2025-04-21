3f8709b5464e3ff339a7a412e479768537f29612
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-04-21 01:41:28 -0400

    Update LightingTemplate.cs

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/Templates/LightingTemplate.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/Templates/LightingTemplate.cs
@@ -8,7 +8,7 @@ static float4 Shade( Material m  )
 {0}
 	for ( int index = 0; index < Light::Count( m.ScreenPosition.xy ); index++ )
 	{{
-		Light light = Light::From( m.ScreenPosition.xy, m.WorldPosition, index);
+		Light light = Light::From( m.ScreenPosition.xy, m.WorldPosition, index );
 {1}
 {2}
 	}}
