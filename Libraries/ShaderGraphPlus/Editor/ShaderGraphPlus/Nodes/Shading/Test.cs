7befed0667241a71a0bb86b7c3dcb2d2ac43c70f
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-04-21 09:43:05 -0400

    UnitAlbedoNode ---> UnitPixelResultInfoNode

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/Test.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/Test.cs
@@ -1,8 +1,8 @@
 ﻿namespace Editor.ShaderGraphPlus.Nodes;
 
 
-[Title( "Unlit Albedo" ), Description( "the result of your pixel shader before lighting." ), Category( "" )]
-public sealed class UnitAlbedoNode : ShaderNodePlus
+[Title( "Unlit Pixel Result Info" ), Description( "the result of your pixel shader before lighting." ), Category( "" )]
+public sealed class UnitPixelResultInfoNode : ShaderNodePlus
 {
 	[Output( typeof( Vector3 ) )]
 	[Hide]
@@ -10,6 +10,14 @@ public sealed class UnitAlbedoNode : ShaderNodePlus
 	public NodeResult.Func ResultA => ( GraphCompiler compiler ) =>
 	{
 		return new NodeResult( ResultType.Vector3, $"m.Albedo", constant: true );
+	};
+
+	[Output( typeof( float ) )]
+	[Hide]
+	[Title( "Opacity" )]
+	public NodeResult.Func ResultB => ( GraphCompiler compiler ) =>
+	{
+		return new NodeResult( ResultType.Float, $"m.Opacity", constant: true );
 	};
 }
 
