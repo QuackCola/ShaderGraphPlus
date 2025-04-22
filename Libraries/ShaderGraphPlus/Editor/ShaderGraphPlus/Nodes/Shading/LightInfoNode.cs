41a604521f605d80a5f590a11b7e339d108f39c4
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-04-21 23:09:18 -0400

    Move these two out of Test.cs

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/LightInfoNode.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/LightInfoNode.cs
@@ -1,26 +1,5 @@
 ﻿namespace Editor.ShaderGraphPlus.Nodes;
 
-
-[Title( "Unlit Pixel Result Info" ), Description( "the result of your pixel shader before lighting." ), Category( "" )]
-public sealed class UnitPixelResultInfoNode : ShaderNodePlus
-{
-	[Output( typeof( Vector3 ) )]
-	[Hide]
-	[Title( "Color" )]
-	public NodeResult.Func ResultA => ( GraphCompiler compiler ) =>
-	{
-		return new NodeResult( ResultType.Vector3, $"m.Albedo", constant: true );
-	};
-
-	[Output( typeof( float ) )]
-	[Hide]
-	[Title( "Opacity" )]
-	public NodeResult.Func ResultB => ( GraphCompiler compiler ) =>
-	{
-		return new NodeResult( ResultType.Float, $"m.Opacity", constant: true );
-	};
-}
-
 [Title( "Light Info" ), Description( "" ), Category( "" )]
 public sealed class LightInfoNode : ShaderNodePlus
 {
---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/UnitPixelResultInfoNode.cs
@@ -0,0 +1,21 @@
+﻿namespace Editor.ShaderGraphPlus.Nodes;
+
+[Title( "Unlit Pixel Result Info" ), Description( "the result of your pixel shader before lighting." ), Category( "" )]
+public sealed class UnitPixelResultInfoNode : ShaderNodePlus
+{
+	[Output( typeof( Vector3 ) )]
+	[Hide]
+	[Title( "Color" )]
+	public NodeResult.Func ResultA => ( GraphCompiler compiler ) =>
+	{
+		return new NodeResult( ResultType.Vector3, $"m.Albedo", constant: true );
+	};
+
+	[Output( typeof( float ) )]
+	[Hide]
+	[Title( "Opacity" )]
+	public NodeResult.Func ResultB => ( GraphCompiler compiler ) =>
+	{
+		return new NodeResult( ResultType.Float, $"m.Opacity", constant: true );
+	};
+}
