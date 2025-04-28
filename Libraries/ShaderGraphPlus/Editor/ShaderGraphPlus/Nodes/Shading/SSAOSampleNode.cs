4e97dfb9012b5171acf57ae484d813874abe505e
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-04-28 08:00:37 -0400

    Add SSAO Sample node

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Shading/SSAOSampleNode.cs
@@ -0,0 +1,20 @@
+﻿namespace Editor.ShaderGraphPlus.Nodes;
+
+[Title( "SSAO Sample" ), Description( "Samples the screen space ambient occlusion" ), Category( "Shading" )]
+public sealed class SSAOSampleNode : ShaderNodePlus
+{
+	[Hide]
+	[Input( typeof( Vector4 ) )]
+	public NodeInput ScreenPosition { get; set; }
+
+
+	[Output( typeof( float ) )]
+	[Hide]
+	[Title( "SSAO" )]
+	public NodeResult.Func Result => ( GraphCompiler compiler ) =>
+	{
+		var screenposition = compiler.ResultOrDefault( ScreenPosition, Vector4.Zero );
+
+		return new NodeResult( ResultType.Float, $"ScreenSpaceAmbientOcclusion::Sample( ${screenposition} )", constant: true );
+	};
+}
