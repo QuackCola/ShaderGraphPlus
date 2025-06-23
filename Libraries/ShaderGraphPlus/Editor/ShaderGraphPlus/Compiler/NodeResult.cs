e414aa58591f4cb25060f65618961a6d0e223b27
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-06-23 01:57:49 -0400

    Tabs tabs...

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/NodeResult.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/NodeResult.cs
@@ -7,11 +7,11 @@ public enum ResultType
 	/// </summary>
 	Bool,
 
-   // Int,
-    /// <summary>
-    /// 1 Component
-    /// </summary>
-    Float,
+	// Int,
+	/// <summary>
+	/// 1 Component
+	/// </summary>
+	Float,
 	/// <summary>
 	/// 2 Component's
 	/// </summary>
@@ -20,22 +20,22 @@ public enum ResultType
 	/// 3 Component's
 	/// </summary>
 	Vector3,
-    /// <summary>
-    /// 4 Component's
-    /// </summary>
-    Color,
-    /// <summary>
-    /// 4 Component's
-    /// </summary>
-    Float2x2,
-    /// <summary>
-    /// 9 Component's
-    /// </summary>
-    Float3x3,
-    /// <summary>
-    /// 16 Component's
-    /// </summary>
-    Float4x4,
+	/// <summary>
+	/// 4 Component's
+	/// </summary>
+	Color,
+	/// <summary>
+	/// 4 Component's
+	/// </summary>
+	Float2x2,
+	/// <summary>
+	/// 9 Component's
+	/// </summary>
+	Float3x3,
+	/// <summary>
+	/// 16 Component's
+	/// </summary>
+	Float4x4,
 	Sampler,
 	TextureObject,
 	String,
@@ -47,59 +47,59 @@ public struct NodeResult : IValid
 {
 	public delegate NodeResult Func( GraphCompiler compiler );
 	public string Code { get; private set; }
-    public ResultType ResultType { get; private set; }
+	public ResultType ResultType { get; private set; }
 	public bool Constant { get; set; }
 	public string[] Errors { get; private init; }
 	public string[] Warnings { get; private init; }
 	public bool IsComponentLess { get; set; }
 	public bool IsDepreciated { get; private set; }
-    public readonly bool IsValid => ResultType > (ResultType)(-1) && !string.IsNullOrWhiteSpace( Code );
+	public readonly bool IsValid => ResultType > (ResultType)(-1) && !string.IsNullOrWhiteSpace( Code );
 	public int VoidComponents { get; private set; }
 
-    public readonly string TypeName
-    {
-        get
-        {
-            //if ( ResultType is ResultType.Int )
-            //{
-            //    return $"float"; // Just identify as a float.
-            //}	
-            if ( ResultType is ResultType.Float )
-            {
-            	return $"float";
-            }
-            else if ( ResultType is ResultType.Vector2 or ResultType.Vector3 or ResultType.Color )
-            {
-            	return $"float{Components()}";
-            }
-            else if ( ResultType is ResultType.Float2x2 )
-            {
-            	return "float2x2";
-            }
-            else if ( ResultType is ResultType.Float3x3 )
-            {
-            	return "float3x3";
-            }
-            else if ( ResultType is ResultType.Float4x4 )
-            {
-            	return "float4x4";
-            }
-            else if ( ResultType is ResultType.Bool )
-            {
-            	return "bool";
-            }
-            else if ( ResultType is ResultType.String )
-            {
-            	return "";
-            }
-            else if (ResultType is ResultType.Gradient)
-            {
-                return "Gradient";
-            }
-            
-            return "float";
-        }
-    }
+	public readonly string TypeName
+	{
+		get
+		{
+			//if ( ResultType is ResultType.Int )
+			//{
+			//	return $"float"; // Just identify as a float.
+			//}	
+			if ( ResultType is ResultType.Float )
+			{
+				return $"float";
+			}
+			else if ( ResultType is ResultType.Vector2 or ResultType.Vector3 or ResultType.Color )
+			{
+				return $"float{Components()}";
+			}
+			else if ( ResultType is ResultType.Float2x2 )
+			{
+				return "float2x2";
+			}
+			else if ( ResultType is ResultType.Float3x3 )
+			{
+				return "float3x3";
+			}
+			else if ( ResultType is ResultType.Float4x4 )
+			{
+				return "float4x4";
+			}
+			else if ( ResultType is ResultType.Bool )
+			{
+				return "bool";
+			}
+			else if ( ResultType is ResultType.String )
+			{
+				return "";
+			}
+			else if (ResultType is ResultType.Gradient)
+			{
+				return "Gradient";
+			}
+
+			return "float";
+		}
+	}
 
 	public readonly Type ComponentType
 	{
@@ -107,38 +107,37 @@ public struct NodeResult : IValid
 		{
 			if (ResultType is ResultType.Void)
 			{
-                return VoidComponents switch
-                {
-                    int r when r == 0 => typeof(bool),
-                    //int r when r == 1 => typeof(int),
-                    int r when r == 1 => typeof(float),
-                    int r when r == 2 => typeof(Vector2),
-                    int r when r == 3 => typeof(Vector3),
-                    int r when r == 4 => typeof(Color),
-                    _ => throw new System.NotImplementedException(),
-                };
-            }
+				return VoidComponents switch
+				{
+					int r when r == 0 => typeof(bool),
+					//int r when r == 1 => typeof(int),
+					int r when r == 1 => typeof(float),
+					int r when r == 2 => typeof(Vector2),
+					int r when r == 3 => typeof(Vector3),
+					int r when r == 4 => typeof(Color),
+					_ => throw new System.NotImplementedException(),
+				};
+			}
 			else
 			{
-                return ResultType switch
-                {
-                    ResultType.Bool => typeof(bool),
-                    //ResultType.Int => typeof(int),
-                    ResultType.Float => typeof(float),
-                    ResultType.Vector2 => typeof(Vector2),
-                    ResultType.Vector3 => typeof(Vector3),
-                    ResultType.Color => typeof(Color),
-                    ResultType.Float2x2 => typeof(Float2x2),
-                    ResultType.Float3x3 => typeof(Float3x3),
-                    ResultType.Float4x4 => typeof(Float4x4),
-                    ResultType.Sampler => typeof(Sampler),
-                    ResultType.TextureObject => typeof(TextureObject),
-                    ResultType.String => typeof(string), // Generic Result
-                    ResultType.Gradient => typeof(Gradient),
-                    _ => throw new System.NotImplementedException(),
-                };
-            }
-
+				return ResultType switch
+				{
+					ResultType.Bool => typeof(bool),
+					//ResultType.Int => typeof(int),
+					ResultType.Float => typeof(float),
+					ResultType.Vector2 => typeof(Vector2),
+					ResultType.Vector3 => typeof(Vector3),
+					ResultType.Color => typeof(Color),
+					ResultType.Float2x2 => typeof(Float2x2),
+					ResultType.Float3x3 => typeof(Float3x3),
+					ResultType.Float4x4 => typeof(Float4x4),
+					ResultType.Sampler => typeof(Sampler),
+					ResultType.TextureObject => typeof(TextureObject),
+					ResultType.String => typeof(string), // Generic Result
+					ResultType.Gradient => typeof(Gradient),
+					_ => throw new System.NotImplementedException(),
+				};
+			}
 		}
 	}
 
@@ -161,43 +160,42 @@ public struct NodeResult : IValid
 	/// </summary>
 	public string Cast( int components, float defaultValue = 0.0f)
 	{
-
-        if ( ResultType is ResultType.Void )
+		if ( ResultType is ResultType.Void )
 		{
-            if ( VoidComponents == components )
-                return Code;
+			if ( VoidComponents == components )
+				return Code;
 
-            if ( VoidComponents > components )
-            {
-                return $"{Code}.{"xyzw"[..components]}";
-            }
-            else if ( VoidComponents == 1 )
-            {
-                return $"float{components}( {string.Join( ", ", Enumerable.Repeat( Code, components ) )} )";
-            }
-            else
-            {
-                return $"float{components}( {Code}, {string.Join( ", ", Enumerable.Repeat( $"{defaultValue}", components - VoidComponents ) )} )";
-            }
-        }
-        else
+			if ( VoidComponents > components )
+			{
+				return $"{Code}.{"xyzw"[..components]}";
+			}
+			else if ( VoidComponents == 1 )
+			{
+				return $"float{components}( {string.Join( ", ", Enumerable.Repeat( Code, components ) )} )";
+			}
+			else
+			{
+				return $"float{components}( {Code}, {string.Join( ", ", Enumerable.Repeat( $"{defaultValue}", components - VoidComponents ) )} )";
+			}
+		}
+		else
 		{
-            if ( Components() == components )
-                return Code;
+			if ( Components() == components )
+			    return Code;
 
-            if ( Components() > components )
-            {
-                return $"{Code}.{"xyzw"[..components]}";
-            }
-            else if ( Components() == 1 )
-            {
-                return $"float{components}( {string.Join( ", ", Enumerable.Repeat( Code, components ) )} )";
-            }
-            else
-            {
-                return $"float{components}( {Code}, {string.Join(", ", Enumerable.Repeat($"{defaultValue}", components - Components()))} )";
-            }
-        }
+			if ( Components() > components )
+			{
+				return $"{Code}.{"xyzw"[..components]}";
+			}
+			else if ( Components() == 1 )
+			{
+				return $"float{components}( {string.Join( ", ", Enumerable.Repeat( Code, components ) )} )";
+			}
+			else
+			{
+				return $"float{components}( {Code}, {string.Join(", ", Enumerable.Repeat($"{defaultValue}", components - Components()))} )";
+			}
+		}
 	}
 
 	/// <summary>
@@ -206,41 +204,41 @@ public struct NodeResult : IValid
 	/// <returns></returns>
 	public readonly int Components()
 	{
-        int components = 0;
+		int components = 0;
+	
+		switch ( ResultType )
+		{
+			//case ResultType.Int:
+			//	components = 1;
+			//	break;
+			case ResultType.Float:
+				components = 1;
+				break;
+			case ResultType.Vector2:
+				components = 2;
+				break;
+			case ResultType.Vector3:
+				components = 3;
+				break;
+			case ResultType.Color:
+				components = 4;
+				break;
+			case ResultType.Float2x2:
+				components = 4;
+				break;
+			case ResultType.Float3x3:
+				components = 9;
+				break;
+			case ResultType.Float4x4:
+				components = 16;
+				break;
+			default:
+				//Log.Warning($"Result type: '{ResultType}' has no components.");
+			break;
+		}
 
-        switch ( ResultType )
-        {
-            //case ResultType.Int:
-            //    components = 1;
-            //    break;
-            case ResultType.Float:
-                components = 1;
-                break;
-            case ResultType.Vector2:
-                components = 2;
-                break;
-            case ResultType.Vector3:
-                components = 3;
-                break;
-            case ResultType.Color:
-                components = 4;
-                break;
-            case ResultType.Float2x2:
-                components = 4;
-                break;
-            case ResultType.Float3x3:
-                components = 9;
-                break;
-            case ResultType.Float4x4:
-                components = 16;
-            	break;
-            default:
-                //Log.Warning($"Result type: '{ResultType}' has no components.");
-            break;
-        }
-        
-        return components;        
-    }
+		return components;
+	}
 
 	public override readonly string ToString()
 	{
