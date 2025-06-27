8ab361a9ecb26b40cc13b9d9d214e80df8cb8dda
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-06-27 01:52:44 -0400

    add internal ConCommands static class

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/ShaderGraphPlusConCommands.cs
@@ -0,0 +1,17 @@
+﻿namespace Editor.ShaderGraphPlus;
+
+internal static class ConCommands
+{
+	public static bool VerboseDebgging { get; internal set; } = false;
+
+	internal static IEnumerable<MainWindow> GetAllShaderGraphPlusWindows()
+	{
+		return Editor.Window.All.OfType<MainWindow>();
+	}
+
+	[ConCmd( "shadergraphplus_verbosedebug" )]
+	public static void CC_VerboseDebugging( bool verboseDebgging )
+	{
+		VerboseDebgging = verboseDebgging;
+	}
+}
