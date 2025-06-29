10a1a5e9b2d83209bede63def2355158e2aabe89
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-06-29 08:23:33 -0400

    Update StaticSwitchNode.cs

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Utility/StaticSwitchNode.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Utility/StaticSwitchNode.cs
@@ -1,10 +1,49 @@
-﻿namespace Editor.ShaderGraphPlus.Nodes;
+﻿using Sandbox.Resources;
+using System.Text;
+
+namespace Editor.ShaderGraphPlus.Nodes;
 
 public enum StaticSwitchMode
 {
 	Create,
 	Reference
 }
+
+[CustomEditor( typeof( string ), NamedEditor = "FeatureReference" )]
+internal sealed class FeatureReferenceControlWidget : DropdownControlWidget<string>
+{ 
+	ShaderGraphPlus Graph;
+	StaticSwitchNode Node;
+
+
+	public FeatureReferenceControlWidget( SerializedProperty property ) : base( property )
+	{
+		Node = property.Parent.Targets.OfType<StaticSwitchNode>().FirstOrDefault();
+		Graph = (ShaderGraphPlus)Node.Graph;
+
+		if ( Graph is null ) return;
+
+		Log.Info("Graph!");
+
+		if ( string.IsNullOrWhiteSpace( SerializedProperty.GetValue<string>() ) )
+		{ 
+			SerializedProperty.SetValue<string>( "" );
+		}
+	}
+
+	protected override IEnumerable<string> GetDropdownValues()
+	{
+		List<string> list = new();
+
+		foreach ( var feature in Graph.FeatureNames )
+		{
+			list.Add( feature );
+		}
+
+		return list;
+	}
+}
+
 
 [Title( "Static Switch" ), Category( "Utility" )]
 public sealed class StaticSwitchNode : ShaderNodePlus
@@ -27,11 +66,16 @@ public sealed class StaticSwitchNode : ShaderNodePlus
 	/// <summary>
 	/// TODO : Implement Reference Mode.
 	/// </summary>
-	[ReadOnly]
+	[Sandbox.ReadOnly]
 	public StaticSwitchMode Mode { get; set; } = StaticSwitchMode.Create;
 
-	[InlineEditor( Label = false ), Group( "Feature" )]
+	[InlineEditor( Label = false ), ShowIf( nameof( Mode ), StaticSwitchMode.Create ), Group( "Feature" )]
 	public ShaderFeature Feature { get; set; } = new();
+	
+	[JsonIgnore]
+	[Title( "Feature" ), ShowIf( nameof( Mode ), StaticSwitchMode.Reference )]
+	[Editor( "FeatureReference" )]
+	public string FeatureReference { get; set; } = "";
 
 	[Output, Hide]
 	public NodeResult.Func Result => ( GraphCompiler compiler ) =>
@@ -42,22 +86,29 @@ public sealed class StaticSwitchNode : ShaderNodePlus
 		if ( compiler.RegisterdFeatureNames.Contains( Feature.FeatureName ) )
 			return NodeResult.Error( $"Feature name `{Feature.FeatureName}` is already registered!" );
 
-		if ( compiler.RegisterShaderFeatureBinary( Feature, out ShaderFeatureInfo shaderFeature ) )
+		if ( Mode is StaticSwitchMode.Create )
 		{
-			if ( compiler.GenerateComboSwitch( shaderFeature, InputTrue, InputFalse, PreviewToggle, out var switchResultVariableName, out var switchBody, out var switchResultType ) )
+			if ( compiler.RegisterShaderFeatureBinary( Feature, out ShaderFeatureInfo shaderFeature ) )
 			{
-				return new NodeResult( switchResultType, switchResultVariableName, switchBody, constant: false );
+				if ( compiler.GenerateComboSwitch( shaderFeature, InputTrue, InputFalse, PreviewToggle, out var switchResultVariableName, out var switchBody, out var switchResultType ) )
+				{
+					return new NodeResult( switchResultType, switchResultVariableName, switchBody, constant: false );
+				}
+				else
+				{
+					return new NodeResult( ResultType.Float, $"{1.0f}" );
+					//return NodeResult.Error( "Switch body could not be generated!" );
+				}
 			}
 			else
 			{
-				return new NodeResult( ResultType.Float, $"{1.0f}" );
-				//return NodeResult.Error( "Switch body could not be generated!" );
+
+				return NodeResult.Error( "Feature Is Invalid!" );
 			}
 		}
 		else
 		{
-
-			return NodeResult.Error( "Feature Is Invalid!", "ass" );
+			return NodeResult.Error( $"Mode `{StaticSwitchMode.Reference}` is not yet implamented" );
 		}
 	};
 }
