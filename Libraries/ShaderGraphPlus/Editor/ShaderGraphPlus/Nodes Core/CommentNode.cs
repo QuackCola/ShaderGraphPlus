5934bb3733c5260e6b8840bc17c627ac66ec9c8b
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-06-29 06:03:14 -0400

    remove global using System.ComponentModel;

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes Core/CommentNode.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes Core/CommentNode.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.ComponentModel;
+
+
 namespace Editor.ShaderGraphPlus;
 
 [Icon( "notes" ), Hide]
--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes Core/Result.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes Core/Result.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.ComponentModel;
+
+
 namespace Editor.ShaderGraphPlus;
 
 /// <summary>
