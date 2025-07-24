521b0cfe8cd2ab0c48faedd974431167b4cd66e7
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-07-23 20:12:25 -0400

    Update JsonUpgraderTest.cs

--- Libraries/ShaderGraphPlus/Editor/JsonUpgraders/JsonUpgraderTest.cs
+++ Libraries/ShaderGraphPlus/Editor/JsonUpgraders/JsonUpgraderTest.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Text.Json.Nodes;
+
+
+//[JsonUpgrader( typeof( ShrimpleCharacterController ), 3 )]
+//private static void UpdateTraceShape( JsonObject json )
+//{
+//	if ( json.Remove( "CylinderTrace", out var newNode ) && newNode is JsonValue boolNode && boolNode.TryGetValue<bool>( out var isCylinder ) )
+//		json["TraceShape"] = isCylinder ? "Cylinder" : "Box";
+//	else
+//		json["TraceShape"] = "Box";
+//}
