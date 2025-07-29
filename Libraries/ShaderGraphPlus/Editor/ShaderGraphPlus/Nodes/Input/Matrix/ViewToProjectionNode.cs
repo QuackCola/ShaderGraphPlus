79110b593fd51642336e673191227cc43d5d9966
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-07-29 08:18:13 -0400

    Create ViewToProjectionNode.cs

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Nodes/Input/Matrix/ViewToProjectionNode.cs
@@ -0,0 +1,15 @@
+﻿namespace ShaderGraphPlus.Nodes;
+
+[Title( "View To Projection" ), Category( "Constants/Matrix" ), Icon( "dataset" )]
+public sealed class ViewToProjectionNode : ShaderNodePlus
+{
+	[Hide, JsonIgnore]
+	public override bool CanPreview => false;
+
+	[Output( typeof( Float4x4 ) ), Title( "Value" )]
+	[Hide]
+	public NodeResult.Func Result => ( GraphCompiler compiler ) =>
+	{
+		return new NodeResult(ResultType.Float4x4, "g_matViewToProjection", true );
+	};
+}
