e845dfbcbbb787faae228bd45341865c6ee202f2
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-08-21 02:36:39 -0400

    Update GraphCompiler.cs

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/GraphCompiler.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Compiler/GraphCompiler.cs
@@ -144,7 +144,7 @@ public sealed partial class GraphCompiler
 		Subgraphs = new();
 		AddSubgraphs( Graph );
 
-		// Set the Initial Vertex and Pixel stage inputs from ShaderTemplate
+		// Set the Initial Vertex and Pixel stage inputs from ShaderTemplate.
 		VertexInputs = ShaderTemplate.VertexInputs;
 		PixelInputs = ShaderTemplate.PixelInputs;
 	}
