64e31b75cbd55d4c36b0966075cecebc07e184ed
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-08-26 14:20:45 -0400

    Update Sound.cs

--- Libraries/ShaderGraphPlus/Editor/Utilities/Sound.cs
+++ Libraries/ShaderGraphPlus/Editor/Utilities/Sound.cs
@@ -4,13 +4,18 @@ namespace ShaderGraphPlus.Utilities;
 
 public static class EdtiorSound
 {
-    public static void OhFiddleSticks()
-    {
-        EditorUtility.PlayRawSound( Editor.FileSystem.Content.GetFullPath( "sounds/editor/kl_fiddlesticks.wav" ) );
-    }
+	public static void OhFiddleSticks()
+	{
+		EditorUtility.PlayRawSound( Editor.FileSystem.Content.GetFullPath( "sounds/editor/kl_fiddlesticks.wav" ) );
+	}
 
-    public static void Success()
-    {
-        EditorUtility.PlayRawSound( Editor.FileSystem.Content.GetFullPath( "sounds/editor/success.wav" ) );
-    }
+	public static void Failure()
+	{
+		EditorUtility.PlayRawSound( Editor.FileSystem.Content.GetFullPath( "sounds/editor/kl_fiddlesticks.wav" ) );
+	}
+
+	public static void Success()
+	{
+		EditorUtility.PlayRawSound( Editor.FileSystem.Content.GetFullPath( "sounds/editor/success.wav" ) );
+	}
 }
