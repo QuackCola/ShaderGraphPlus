3812feba2e3b9c9cf193e194656a127ba1ac0a69
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-09-29 21:13:43 -0400

    Start consolidating all the upgrader methods and stuff
    I don't know if this is a good idea but i can revert it if its fucking stupid later.

---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Upgraders/Nodes/NodeVersions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Nodes;
+using ShaderGraphPlus.Nodes;
+
+namespace ShaderGraphPlus;
+
+public record NodeVersionRegistryEntry( Type type, int version );
+
+/// <summary>
+/// TODO : See if is even a good idea to store the current node versions here.
+/// </summary>
+internal class NodeVersions
+{ 
+	internal static List<NodeVersionRegistryEntry> NodeVersionRegistry { get; private set; }
+
+	static NodeVersions()
+	{
+		Update();
+	}
+
+	[Event( "hotloaded" )]
+	static void Update()
+	{
+		NodeVersionRegistry = new()
+		{
+			{ new NodeVersionRegistryEntry( typeof( Bool ), 2 )},
+			{ new NodeVersionRegistryEntry( typeof( Int ), 2 )},
+			{ new NodeVersionRegistryEntry( typeof( Float ), 2 )},
+			{ new NodeVersionRegistryEntry( typeof( Float2 ), 2 )},
+			{ new NodeVersionRegistryEntry( typeof( Float3 ), 2 )},
+			{ new NodeVersionRegistryEntry( typeof( Float4 ), 2 )},
+		};
+	}
+
+	internal static void RegisterUserNodes()
+	{
+		// TODO
+	}
+}
---
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/Upgraders/Nodes/ParameterNodes/ParameterNodeUpgraders.cs
@@ -0,0 +1,125 @@
+﻿using System.Text.Json.Nodes;
+using ShaderGraphPlus.Nodes;
+
+namespace ShaderGraphPlus;
+
+internal static class ParameterNodeUpgraders
+{
+	private static void SetEnumTypeUpgrader_v2( JsonObject json )
+	{
+		var name = json["Name"].ToString();
+		var nodeType = ParameterNodeModeType.Constant;
+
+		if ( !string.IsNullOrWhiteSpace( name ) )
+		{
+			nodeType = ParameterNodeModeType.Property;
+		}
+		else
+		{
+		}
+
+		json[nameof( IParameterNode.ParameterNodeType )] = JsonSerializer.SerializeToNode( nodeType, ShaderGraphPlus.SerializerOptions() );
+	}
+
+	[SGPJsonUpgrader( typeof( Bool ), 2 )]
+	public static void BoolNodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+
+	[SGPJsonUpgrader( typeof( Int ), 2 )]
+	public static void IntNodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+
+	[SGPJsonUpgrader( typeof( Float ), 2 )]
+	public static void FloatNodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+
+	[SGPJsonUpgrader( typeof( Float2 ), 2 )]
+	public static void Float2NodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+
+	[SGPJsonUpgrader( typeof( Float3 ), 2 )]
+	public static void Float3NodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+
+	[SGPJsonUpgrader( typeof( Float4 ), 2 )]
+	public static void Float4NodeUpgrader_v2( JsonObject json )
+	{
+		if ( !json.ContainsKey( "Name" ) )
+		{
+			return;
+		}
+
+		try
+		{
+			SetEnumTypeUpgrader_v2( json );
+		}
+		catch
+		{
+		}
+	}
+}
