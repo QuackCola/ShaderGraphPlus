31394ebbd85c0b364d0f91103fcc2eb21be8a1db
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-10-01 23:38:19 -0400

    Account for quack.shadergraphplus_staging library path

--- Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/ShaderGraphPlusFileSystem.cs
+++ Libraries/ShaderGraphPlus/Editor/ShaderGraphPlus/ShaderGraphPlusFileSystem.cs
@@ -2,37 +2,61 @@
 
 internal static class ShaderGraphPlusFileSystem
 {
-    public static BaseFileSystem Root => Editor.FileSystem.Libraries.CreateSubSystem($"/{GetLibraryFolderName()}");
-    public static BaseFileSystem Content => Editor.FileSystem.Libraries.CreateSubSystem($"/{GetLibraryFolderName()}/Assets");
+	public static BaseFileSystem Root => Editor.FileSystem.Libraries.CreateSubSystem($"/{GetLibraryFolderPath()}");
+	public static BaseFileSystem Content => Editor.FileSystem.Libraries.CreateSubSystem($"/{GetLibraryFolderPath()}/Assets");
 
-    /// <summary>
-    /// Get's the folder name of the Shader Graph Plus library.
-    /// </summary>
-    /// <returns></returns>
-    private static string GetLibraryFolderName()
-    {
-        var dev_name = "ShaderGraphPlus";
-        var user_name = "quack.shadergraphplus";
-        var dev_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{dev_name}";
-        var user_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{user_name}";
+	/// <summary>
+	/// Get's the folder name of the Shader Graph Plus library.
+	/// </summary>
+	/// <returns></returns>
+	private static string GetLibraryFolderPath()
+	{
+		var dev_name = "ShaderGraphPlus";
+		var user_staging = "quack.shadergraphplus_staging";
+		var user_name = "quack.shadergraphplus";
+		var dev_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{dev_name}";
+		var user_staging_path = $"{Project.Current.GetRootPath().Replace( '\\', '/' )}/Libraries/{user_staging}";
+		var user_path = $"{Project.Current.GetRootPath().Replace('\\', '/')}/Libraries/{user_name}";
 
-        if (Directory.Exists(dev_path))
-        {
-            return dev_name;
-        }
-        else if (Directory.Exists(user_path))
-        {
-            return user_name;
-        }
-        else if (Directory.Exists(dev_path) || Directory.Exists(user_path))
-        {
-            Utilities.EdtiorSound.OhFiddleSticks();
-            Log.Error($"Both dev_path & user_path exist!");
-            return null;
-        }
-        else
-        {
-            return null; // Neither path exists
-        }
-    }
+		var targetPath = "";
+
+		if ( Directory.Exists( dev_path ) )
+		{
+			targetPath = dev_name;
+		}
+		else if ( Directory.Exists( user_staging_path ) )
+		{
+			if ( !string.IsNullOrWhiteSpace( targetPath ) )
+			{
+				Utilities.EdtiorSound.OhFiddleSticks();
+
+				Log.Error( $"user_staging_path \"{user_staging_path}\" was found but we already selected a path!. Bailing!!!!" );
+
+				return null;
+			}
+
+			targetPath = user_staging_path;
+		}
+		else if ( Directory.Exists( user_path ))
+		{
+			if ( !string.IsNullOrWhiteSpace( targetPath ) )
+			{
+				Utilities.EdtiorSound.OhFiddleSticks();
+				
+				Log.Error( $"user_path \"{user_path}\" was found but we already selected a path!. Bailing!!!!" );
+
+				return null;
+			}
+
+			targetPath = user_name;
+		}
+
+		if ( !string.IsNullOrWhiteSpace( targetPath ) )
+		{
+			return targetPath;
+		}
+
+		// No valid path exists.
+		return null;
+	}
 }
