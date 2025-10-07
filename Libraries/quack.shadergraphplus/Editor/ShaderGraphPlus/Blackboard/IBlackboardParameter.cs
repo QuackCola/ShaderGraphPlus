d1d2a6a00144c65ee113f244484be77fac87856e
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-10-07 09:03:00 -0400

    Update IBlackboardParameter.cs

--- Libraries/quack.shadergraphplus/Editor/ShaderGraphPlus/Blackboard/IBlackboardParameter.cs
+++ Libraries/quack.shadergraphplus/Editor/ShaderGraphPlus/Blackboard/IBlackboardParameter.cs
@@ -3,8 +3,8 @@ namespace ShaderGraphPlus;
 
 internal interface IBlackboardParameter
 {
-	public Guid Identifier { get; }
+	Guid Identifier { get; }
 	string Name { get; set; }
 
-	Color GetTypeColor( BlackboardView view );
+	//Color GetTypeColor( BlackboardView view );
 }
