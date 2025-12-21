a38d5423b3210a13229201904dd4f65998368953
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-12-21 18:40:36 -0500

    Update ShaderTemplate.cs

--- Libraries/quack.shadergraphplus/Editor/ShaderGraphPlus/Compiler/Templates/ShaderTemplate.cs
+++ Libraries/quack.shadergraphplus/Editor/ShaderGraphPlus/Compiler/Templates/ShaderTemplate.cs
@@ -67,8 +67,6 @@ PS
 }}
 ";
 
-
-
 	// Included by common/vertexinput.hlsl
 	internal static Dictionary<string, string> InternalVertexInputs => new()
 	{
@@ -151,45 +149,45 @@ return ShadingModelStandard::Shade( m );";
 	public static string TextureDefinition => @"<!-- dmx encoding keyvalues2_noids 1 format vtex 1 -->
 ""CDmeVtex""
 {{
-    ""m_inputTextureArray"" ""element_array"" 
-    [
-        ""CDmeInputTexture""
-        {{
-            ""m_name"" ""string"" ""0""
-            ""m_fileName"" ""string"" ""{0}""
-            ""m_colorSpace"" ""string"" ""{1}""
-            ""m_typeString"" ""string"" ""2D""
-            ""m_imageProcessorArray"" ""element_array"" 
-            [
-                ""CDmeImageProcessor""
-                {{
-                    ""m_algorithm"" ""string"" ""{3}""
-                    ""m_stringArg"" ""string"" """"
-                    ""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
-                }}
-            ]
-        }}
-    ]
-    ""m_outputTypeString"" ""string"" ""2D""
-    ""m_outputFormat"" ""string"" ""{2}""
-    ""m_textureOutputChannelArray"" ""element_array""
-    [
-        ""CDmeTextureOutputChannel""
-        {{
-            ""m_inputTextureArray"" ""string_array""
-            [
-                ""0""
-            ]
-            ""m_srcChannels"" ""string"" ""rgba""
-            ""m_dstChannels"" ""string"" ""rgba""
-            ""m_mipAlgorithm"" ""CDmeImageProcessor""
-            {{
-                ""m_algorithm"" ""string"" ""Box""
-                ""m_stringArg"" ""string"" """"
-                ""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
-            }}
-            ""m_outputColorSpace"" ""string"" ""{1}""
-        }}
-    ]
+	""m_inputTextureArray"" ""element_array"" 
+	[
+		""CDmeInputTexture""
+		{{
+			""m_name"" ""string"" ""0""
+			""m_fileName"" ""string"" ""{0}""
+			""m_colorSpace"" ""string"" ""{1}""
+			""m_typeString"" ""string"" ""2D""
+			""m_imageProcessorArray"" ""element_array"" 
+			[
+				""CDmeImageProcessor""
+				{{
+					""m_algorithm"" ""string"" ""{3}""
+					""m_stringArg"" ""string"" """"
+					""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
+				}}
+			]
+		}}
+	]
+	""m_outputTypeString"" ""string"" ""2D""
+	""m_outputFormat"" ""string"" ""{2}""
+	""m_textureOutputChannelArray"" ""element_array""
+	[
+		""CDmeTextureOutputChannel""
+		{{
+			""m_inputTextureArray"" ""string_array""
+			[
+				""0""
+			]
+			""m_srcChannels"" ""string"" ""rgba""
+			""m_dstChannels"" ""string"" ""rgba""
+			""m_mipAlgorithm"" ""CDmeImageProcessor""
+			{{
+				""m_algorithm"" ""string"" ""Box""
+				""m_stringArg"" ""string"" """"
+				""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
+			}}
+			""m_outputColorSpace"" ""string"" ""{1}""
+		}}
+	]
 }}";
 }
